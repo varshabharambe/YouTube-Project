9e4f845523b69f0e8300ffade86926240ec7d722
Varsha <bharambevarsha13@gmail.com>
2024-04-18 23:45:48 +0530

    Main page structure built

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Body from './components/Body';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="text-3xl font-bold underline">
-      Hello...!
+    <div className="">
+      <Header/>
+      <Body/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1 justify-between">
+        <img
+          className="h-8"
+          src="https://cdn.iconscout.com/icon/free/png-256/free-hamburger-menu-462145.png?f=webp"
+          alt="menu"
+        />
+        <img
+          className="h-8 mx-2"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png"
+          alt="logo"
+        />
+      </div>
+      <div className="col-span-10">
+        <input type="text" />
+        <button>Search</button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvI2swFo0rVijznXtvtB_bSRn0ZWA09JRHUbyDYJcrra5SWbE8Rbq9nMX0RUAnoqG6xic&usqp=CAU"
+          alt="User Icon"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
